f2f5e55e43088a82ec39e747416f733807116d30
ElliotPi <103081209+ElliotPi@users.noreply.github.com>
2024-02-09 23:48:28 -0700

    Add project files.

---
+++ Hobby/wwwroot/js/TutorCalculator.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    $('#calculateBtn').click(function () {
+        var hours = parseFloat($('#hours').val());
+
+        if (!isNaN(hours) && hours > 0)
+        {
+            var hourlyRate = parseFloat($('#hourlyRate').val().replace('$', ''));
+            var total = hours * hourlyRate;
+
+            $('#total').val('$' + total.toFixed(2));
+        }
+        else
+        {
+            alert('Please enter a valid number of hours.');
+        }
+    });
+});
